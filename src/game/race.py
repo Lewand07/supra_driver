38620b24e0398ecbeb6dd3b983ae0db140520038
lewanwoj <wojciech.lewandowski@tietoevry.com>
2023-01-31 00:55:11 +0100

    Add enemies

--- src/game/race.py
+++ src/game/race.py
@@ -1,5 +1,6 @@
 import pygame
 import math
+import random
 import src.game.racers as racers
 from src.constants import (
     FRAME_RATE,
@@ -23,14 +24,25 @@ class Race():
         self.__clock = pygame.time.Clock()
         self.__player = player
         self.__playerGroup = pygame.sprite.Group()
+        self.__enemies = enemies
         self.__enemiesGroup = pygame.sprite.Group()
         self.__background = roadImg
         self.__backgroundRect = roadImg.get_rect()
         self.__playerGroup.add(self.__player)
-        
+        self.__font = pygame.font.Font(pygame.font.get_default_font(), 60)
         screen.blit(self.__background, self.__backgroundRect)
         self.race(screen)
 
+    def addEnemy(self) -> None:
+        image = random.choice(self.__enemies)
+        x = math.floor((ROADSIDE_RIGHT - ROADSIDE_LEFT)* random.random() + ROADSIDE_LEFT)
+        enemy = racers.Enemy(image, x, 0)
+        self.__enemiesGroup.add(enemy)
+
+    def printScore(self, screen: pygame.Surface) -> None:
+        text = "Score: " + str(self.__score)
+        txt = self.__font.render(text, True, pygame.color.Color("white"))
+        screen.blit(txt, (0, 0))
 
     def race(self, screen : pygame.Surface) -> None:
         """
@@ -41,14 +53,14 @@ class Race():
         i = 0
         scroll = 0
         
-        
-        
         while self.__run:
             self.__clock.tick(FRAME_RATE)
             
             # draw objects
             screen.blit(self.__background, self.__backgroundRect)
             self.__playerGroup.draw(screen)
+            self.__enemiesGroup.draw(screen)
+            self.printScore(screen)
             pygame.display.flip()
 
             # handle keys
@@ -64,7 +76,7 @@ class Race():
             if (keys[pygame.K_UP] or keys[pygame.K_w]) and self.__player.rect.y > 0:
                 self.__player.rect.top -= 2 * SPEED
             if (keys[pygame.K_DOWN] or keys[pygame.K_s]) and self.__player.rect.y < ROAD_HEIGHT:
-                self.__player.rect.top += SPEED
+                self.__player.rect.bottom += SPEED
 
             # scroll road
             if self.__player.rect.y < ROAD_HEIGHT:
@@ -74,6 +86,16 @@ class Race():
             self.__backgroundRect.y = i * bg_height - scroll
             scroll -= SPEED 
             i -= 1
+
+            # spawn enemies
+            if len(self.__enemiesGroup) < 2:
+                self.addEnemy()
+            
+            for enemy in self.__enemiesGroup:
+                enemy.rect.y += SPEED
+                if enemy.rect.y >= WINDOW_HEIGHT:
+                    enemy.kill()
+                    self.__score += 1
 
             while self.__gameOver:
                 self.__clock.tick(FRAME_RATE)
@@ -88,7 +110,7 @@ class Race():
                             # reset the game
                             self.__gameOver = False
                             self.__score = 0
-                            #vehicle_group.empty()
+                            #self.__enemiesGroup.empty()
                             self.__player.rect.center = [0, 0]
                         elif event.key == pygame.K_n:
                             # exit the loops
